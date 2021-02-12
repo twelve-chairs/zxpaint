0537b9fade7ced989851d24fee9310f0465ae080
vokamis <asimakov@twelvechairs-software.com>
2021-02-12 17:40:20 -0500

    Fix frame rate

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ if [ "$(uname)" == "Darwin" ]; then
   conan install .
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   conan profile update settings.compiler.libcxx=libstdc++11 default
-  conan install . --build=spdlog
+  conan install . --build=spdlog --build=fmt
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     # Do something under 32 bits Windows NT platform
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
