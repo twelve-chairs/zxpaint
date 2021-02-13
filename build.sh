148d25fe7824e340a47d54028f037637f3f55f40
Twelve Chairs Software, LLC <56702533+twelvechairssoftware@users.noreply.github.com>
2021-02-13 10:57:59 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,25 +1,27 @@
 conan profile new default --detect
 
 if [ "$(uname)" == "Darwin" ]; then
+  echo "Darwin detected"
   conan profile update settings.compiler.libcxx=libc++ default
-  conan install .
 
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  echo "Linux detected"
   conan profile update settings.compiler.libcxx=libstdc++11 default
-  conan install . --build=spdlog --build=fmt
 
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    # Do something under 32 bits Windows NT platform
-    echo
+  # Do something under 32 bits Windows NT platform
+  echo "WIN32 Detected"
 
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    # Do something under 64 bits Windows NT platform
-    echo
+  # Do something under 64 bits Windows NT platform
+  echo "WIN64 Detected"
 
 else
-    # Default
-    echo
+  # Default
+  echo "Unable to detect platform"
 fi
+
+conan install . --build=spdlog --build=fmt
 
 mkdir cmake-build-minsizerel
 
